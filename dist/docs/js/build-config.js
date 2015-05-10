a4361abadb4908f8c4d5888ef510ed128584cfe8
ad-uistyleguide <no411xs@gmail.com>
2015-05-10 01:11:31 -0700

    Up and running latest Material Angular locally

---
+++ dist/docs/js/build-config.js
@@ -0,0 +1,7 @@
+DocsApp.constant('BUILDCONFIG', {
+  "ngVersion": "1.3.15",
+  "version": "0.9.0",
+  "repository": "https://github.com/angular/material",
+  "commit": "5f3520e17f7472f45b4db1d851394adebbb34892",
+  "date": "2015-05-09 14:57:30 -0700"
+});
