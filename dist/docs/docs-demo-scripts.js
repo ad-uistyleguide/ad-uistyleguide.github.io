eac78662d3e324b958e7a99f25284af1ccbce948
ad-uistyleguide <no411xs@gmail.com>
2015-05-19 16:25:00 -0700

    Revert "Updated a’s and content"
    This reverts commit 793895c00bde798a3b120061891598c02f04b071.

--- dist/docs/docs-demo-scripts.js
+++ dist/docs/docs-demo-scripts.js
@@ -708,15 +708,6 @@ angular
   });
 
 
-angular.module('appSvgIconSets', ['ngMaterial'])
-  .controller('DemoCtrl', function($scope) {})
-  .config(function($mdIconProvider) {
-    $mdIconProvider
-      .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
-      .defaultIconSet('img/icons/sets/core-icons.svg', 24);
-  });
-
-
 angular.module('appDemoSvgIcons', ['ngMaterial'])
 .controller('DemoCtrl', function( $scope ) {
 
@@ -725,6 +716,15 @@ angular.module('appDemoSvgIcons', ['ngMaterial'])
       return 'img/icons/android.svg';
     }
 });
+
+
+angular.module('appSvgIconSets', ['ngMaterial'])
+  .controller('DemoCtrl', function($scope) {})
+  .config(function($mdIconProvider) {
+    $mdIconProvider
+      .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
+      .defaultIconSet('img/icons/sets/core-icons.svg', 24);
+  });
 
 
 angular.module('appUsingTemplateCache', ['ngMaterial'])
@@ -1359,9 +1359,9 @@ app.controller('AppCtrl', function($scope) {
   }
 });
 
+angular.module('whiteframeBasicUsage', ['ngMaterial']);
+
 angular.module('tooltipDemo1', ['ngMaterial'])
 .controller('AppCtrl', function($scope) {
   $scope.demo = {};
 });
-
-angular.module('whiteframeBasicUsage', ['ngMaterial']);
