c50813f50ae6f7687e0832a3b0fb16de5583a4cc
ad-uistyleguide <no411xs@gmail.com>
2015-05-19 14:59:12 -0700

    Removed Red from Content Area

--- dist/docs/docs.js
+++ dist/docs/docs.js
@@ -724,8 +724,8 @@ DocsApp.constant('BUILDCONFIG', {
   "ngVersion": "1.3.15",
   "version": "0.9.0",
   "repository": "https://github.com/angular/material",
-  "commit": "38227fbd0a03b4dec1f09e5d0ea0d71c7bdb2502",
-  "date": "2015-05-11 13:36:41 -0700"
+  "commit": "624326dab673eb9ab9e76ee8c0bd8ff49f9e2d3d",
+  "date": "2015-05-18 17:10:17 -0700"
 });
 
 (function() {
