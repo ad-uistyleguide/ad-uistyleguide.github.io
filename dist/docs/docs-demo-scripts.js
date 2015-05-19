793895c00bde798a3b120061891598c02f04b071
ad-uistyleguide <no411xs@gmail.com>
2015-05-19 14:17:10 -0700

    Updated a’s and content

--- dist/docs/docs-demo-scripts.js
+++ dist/docs/docs-demo-scripts.js
@@ -708,6 +708,15 @@ angular
   });
 
 
+angular.module('appSvgIconSets', ['ngMaterial'])
+  .controller('DemoCtrl', function($scope) {})
+  .config(function($mdIconProvider) {
+    $mdIconProvider
+      .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
+      .defaultIconSet('img/icons/sets/core-icons.svg', 24);
+  });
+
+
 angular.module('appDemoSvgIcons', ['ngMaterial'])
 .controller('DemoCtrl', function( $scope ) {
 
@@ -716,15 +725,6 @@ angular.module('appDemoSvgIcons', ['ngMaterial'])
       return 'img/icons/android.svg';
     }
 });
-
-
-angular.module('appSvgIconSets', ['ngMaterial'])
-  .controller('DemoCtrl', function($scope) {})
-  .config(function($mdIconProvider) {
-    $mdIconProvider
-      .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
-      .defaultIconSet('img/icons/sets/core-icons.svg', 24);
-  });
 
 
 angular.module('appUsingTemplateCache', ['ngMaterial'])
@@ -1359,9 +1359,9 @@ app.controller('AppCtrl', function($scope) {
   }
 });
 
-angular.module('whiteframeBasicUsage', ['ngMaterial']);
-
 angular.module('tooltipDemo1', ['ngMaterial'])
 .controller('AppCtrl', function($scope) {
   $scope.demo = {};
 });
+
+angular.module('whiteframeBasicUsage', ['ngMaterial']);
